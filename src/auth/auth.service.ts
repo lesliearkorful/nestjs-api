dc77d4eb27b2999b76f8a2eb19831168cd0b78b1
Leslie Arkorful <lesliearkorful@gmail.com>
2020-08-11 19:36:02 +0000

    Hash passwords

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../users/user.entity';
 import { Repository } from 'typeorm';
 import { JwtService } from '@nestjs/jwt';
+import bcrypt = require('bcrypt');
 
 @Injectable()
 export class AuthService {
@@ -12,20 +13,25 @@ export class AuthService {
     private jwtService: JwtService,
   ) { }
 
+  private _hashPassword(password: string): string {
+    const salt = bcrypt.genSaltSync(10);
+    return bcrypt.hashSync(password, salt);
+  }
+
   private async _updateToken(id: number, token: string) {
     const update = await this.usersRepository.update(id, { token: token });
     return update.affected > 0;
   }
 
   async validateUser(payload: any): Promise<User> {
-    const user =  await this.usersRepository.findOne({ where: { username: payload.username } });
+    const user = await this.usersRepository.findOne({ where: { username: payload.username } });
     if (user.token.length == 0) return null;
     return user;
   }
 
   async login(username: string, password: string): Promise<User> {
-    const user = await this.usersRepository.findOne({ username: username, password: password });
-    if (user?.username == username && user?.password == password) {
+    const user = await this.usersRepository.findOne({ username: username });
+    if (user?.username == username && bcrypt.compareSync(password, user.password)) {
       const payload = { username: user.username, sub: user.id };
       const token = this.jwtService.sign(payload);
       await this._updateToken(user.id, token);
@@ -44,6 +50,8 @@ export class AuthService {
     if (checkEmail.length > 0) throw new ConflictException("Email already exists");
     const checkUsername = await this.usersRepository.find({ where: { username: user.username } });
     if (checkUsername.length > 0) throw new ConflictException("Username already exists");
+    const hash = this._hashPassword(user.password);
+    user.password = hash;
     return this.usersRepository.save(user);
   }
 }
